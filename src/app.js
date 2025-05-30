fbeb4b07c58a3ee8b96d887e7ab258d920cce0c1
Juanca-Mj <Jcmejiasanchez05@gmail.com>
2025-05-30 15:56:50 -0500

    enrutador y controllador

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import categoriasRoutes from './routes/categorias.routes.js';
 
 const app = express();
 
 app.set("port", process.env.PORT || 4000);
+
+app.use("/api/categorias", categoriasRoutes);
 
 export default app;
---
+++ src/controllers/categoria.controllers.js
@@ -0,0 +1,7 @@
+const getCategorias = (req, res) => {
+    res.json({ "Categorias": "cerveza" });
+};
+
+export const methodHTTP = {
+    getCategorias
+};
