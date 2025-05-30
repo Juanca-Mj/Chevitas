8ed4b422a1b5d8b3e17d5bc1f7c88d9251759517
Juanca-Mj <Jcmejiasanchez05@gmail.com>
2025-05-30 17:02:03 -0500

    Conexion a la base de datos HTTP

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,5 +1,13 @@
-const getCategorias = (req, res) => {
-    res.json({ "Categorias": "cerveza" });
+import getConnection from "../db/database.js";
+
+const getCategorias = async (req, res) => {
+    try{
+    const connection = await getConnection();
+    const result = await connection.query("SELECT id, nombre, descripcion FROM categorias");
+    res.json(result);
+   } catch (error) {
+       res.status(500).json({ error: "Error en la consulta" });
+   }
 };
 
 export const methodHTTP = {
